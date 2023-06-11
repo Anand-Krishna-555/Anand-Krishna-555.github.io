c89323472dde8141ec1f08e460a5044333e71c63
Krishna <99634559+Anand-Krishna-555@users.noreply.github.com>
2023-06-11 22:12:37 +0530

    Update smartdxWebSdkConfig.js

--- js/smartdxWebSdkConfig.js
+++ js/smartdxWebSdkConfig.js
@@ -10,4 +10,14 @@ document.addEventListener("DOMContentLoaded", ()=>{
 	script.src="https://anand-krishna-555.github.io/ServerPath.github.io/handlers/f4f0811b658c45e794322f307b93c235.js";
 	script.setAttribute("fcm_service_path","firebase-messaging-sw.js");
     document.head.appendChild(script);
+	window.ReWebSDKConfig = {
+		"customFileBase" : "https://anand-krishna-555.github.io/ServerPath.github.io"
+	}
+
+	// var resulSdk = document.createElement('script')
+	// resulSdk.type = 'text/javascript';
+	// resulSdk.src = 'http://127.0.0.1:5500/common/handlers/bb073c1c05894bc3bb5deaf9ef5364e5.js'
+
+	// resulSdk.defer = true
+	// document.head.appendChild(resulSdk)
 })
